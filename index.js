42b818702af5d9227d539028af2dc72ebae31566
Joakim Björklind SOFTK20P <joakim.bjorklind@centria.fi>
2020-10-08 12:49:17 +0300

    exercise 3.3 ok!

--- index.js
+++ index.js
@@ -33,6 +33,17 @@ app.get('/info', (req, res) => {
     const date = Date()
     res.send(`<p>${info}</p><p>${date}</p>`)
 })
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
