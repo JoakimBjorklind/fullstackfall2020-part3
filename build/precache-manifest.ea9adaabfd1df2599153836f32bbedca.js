ffc64e7be4627f5009d7d228d920acbdc11269eb
Joakim Bjorklind <joakim.bjorklind@centria.fi>
2020-10-14 10:27:59 +0300

    uibuild

---
+++ build/precache-manifest.ea9adaabfd1df2599153836f32bbedca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba8f796861ed7639c6db6f7952659b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f17e047505a917669080",
+    "url": "/static/css/main.dd15ef87.chunk.css"
+  },
+  {
+    "revision": "54dd32709c192453a110",
+    "url": "/static/js/2.d98781c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d98781c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f17e047505a917669080",
+    "url": "/static/js/main.e1525d8f.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
