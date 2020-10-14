60bbcc9288a6da7a31f8096853b9bb4d37394368
Joakim Bjorklind <joakim.bjorklind@centria.fi>
2020-10-14 11:05:51 +0300

    PORT mistake fixed

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.delete('/api/persons/:id', (req, res) => {
   
     res.json(person)
   })
-const PORT = process.env.Port || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
